7751d2a222c81151473e69327cdb143d756cb9cb
jinmook <jink0608@naver.com>
2021-09-29 21:55:28 +0900

    add wise-saying

---
+++ javascript/wiseSaying.js
@@ -0,0 +1,37 @@
+// 랜덤으로 명언 선택해주는 파일
+const wiseSayingDiv = document.querySelector("#wise-saying");
+
+const wiseSaying = [
+    {
+        saying: "Be gentle with the young.",
+        name: "Juvenal"
+    },
+    {
+        saying: "Life is just one damned thing after another",
+        name: "Elbert Hubbard"
+    },
+    {
+        saying: "Wish not so much to live long as to live well.",
+        name: "Benjamin Franklin"
+    },
+    {
+        saying: "Life is a long lesson in humility.",
+        name: "James M. Barrie"
+    },
+    {
+        saying: "The unexamined life is not worth living.",
+        name: "Socrates"
+    }
+]
+
+const index = Math.floor(Math.random() * wiseSaying.length)
+
+const pSaying = document.createElement("p");
+pSaying.textContent = wiseSaying[index].saying;
+const pName = document.createElement("p");
+pName.textContent = wiseSaying[index].name;
+
+wiseSayingDiv.append(pSaying);
+wiseSayingDiv.append(pName);
+
+
